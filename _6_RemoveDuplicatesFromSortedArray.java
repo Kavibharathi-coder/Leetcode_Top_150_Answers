aa02c9f0751e068e09880d91ef4a68ebf2d2c1d3
Kavibharathi-coder <kavibharathi1807@gmail.com>
2023-10-14 19:53:25 +0530

    Initial Commit for this project

--- _6_RemoveDuplicatesFromSortedArray.java
+++ _6_RemoveDuplicatesFromSortedArray.java
@@ -2,6 +2,19 @@ package TopInterviewQuestions;
 
 public class _6_RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(arr));
+    }
 
+    private static int removeDuplicates(int[] arr) {
+        int j = 1;
+
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] != arr[i + 1])
+            {
+                arr[j++] = arr[i + 1];
+            }
+        }
+        return j;
     }
 }
