8795b0b40b215d1fd1127d6e5b73c88b383a6575
Kavibharathi-coder <kavibharathi1807@gmail.com>
2023-10-15 06:12:02 +0530

    Initial Commit for this project

---
+++ _9_Sqrt.java
@@ -0,0 +1,30 @@
+package TopInterviewQuestions;
+
+public class _9_Sqrt {
+    public static void main(String[] args) {
+        int num = 8;
+        System.out.println(squareRoot(num));
+    }
+
+    private static int squareRoot(int num) {
+        long left = 0;
+        long right = num;
+
+        while(left + 1 < right){
+            long mid = left + (right - left) / 2;
+
+            if(mid * mid == num) return (int)mid;
+
+            else if(mid * mid < num){
+                left = mid;
+            }
+            else{
+                right = mid;
+            }
+        }
+        if(right * right == num){
+            return (int)right;
+        }
+        return (int)left;
+    }
+}
